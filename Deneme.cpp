284005320629d14e5ea32a08e05b37370a22da4f
ufukkayabasi <trufukkayabasi@gmail.com>
2023-11-23 21:15:17 +0300

    first commit

---
+++ Deneme.cpp
@@ -0,0 +1,15 @@
+
+
+#include <iostream>
+
+#include <fstream>
+#include <cassert>
+#include <vector>
+int main() {
+
+
+
+    std::cout << "Test" << std::endl;
+
+    return 0;
+}
---
+++ build/CMakeFiles/Deneme.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Deneme.dir/Deneme.cpp.obj"
+  "CMakeFiles/Deneme.dir/Deneme.cpp.obj.d"
+  "Deneme.exe"
+  "Deneme.exe.manifest"
+  "Deneme.pdb"
+  "libDeneme.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Deneme.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
